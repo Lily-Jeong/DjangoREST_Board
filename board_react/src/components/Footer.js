e62d8c3d89577edd0ec01d322eaf57f36f4e1247
Lily Jeong <superfreak.co.kr@gmail.com>
2024-06-11 19:29:54 +0900

    react setup - done

---
+++ board_react/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../shared/App.css";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>2024 @LilyJeong, All rights reserved</p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ board_react/src/shared/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import {
+  Main,
+  Like,
+  My,
+  Login,
+  Detail,
+  Register,
+  New,
+  Profile,
+} from "../pages/index";
+import { Header, Footer, Menu } from "../components/index";
+import "./App.css";
+
